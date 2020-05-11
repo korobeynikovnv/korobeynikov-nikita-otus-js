c36d9569de72783636bac6c1c103540f43b7b3d8
Nikita <korobeynikov.nv@gmail.com>
2020-05-11 19:26:26 +1000

    Добавлен скрипт для просмотра содержимого папки.

---
+++ javascript-6/tree.js
@@ -0,0 +1,62 @@
+import minimist from 'minimist';
+import { resolve, relative, parse, extname } from 'path';
+import { readdir} from 'fs';
+import { promisify } from 'util'
+const readDirASync = promisify(readdir);
+const args = minimist(process.argv);
+const fullPath = resolve(args['path']);
+
+/**
+ * viewModel для просмотра содержимого папки
+ * @typedef {Object} Content
+ * @property {String[]} files
+ * @property {String[]} folders
+ */
+
+/**
+ * Вывести в консоль содержимое папки рекурсивно
+ * @param {String} path 
+ */
+async function getContent(path){
+	let content = await readFolder(path, path);
+
+	console.log(JSON.stringify(content));
+}
+
+/**
+ * Считать содержимое папки рекурсивно
+ * @param {String} current 
+ * @param {String} root
+ * @return {Promise<Content>}
+ */
+async function readFolder(current, root){
+
+	/** @type{Content} */
+	let content = {
+		files: [],
+		folders : []
+	}
+
+	let results = await readDirASync(current);
+	for (let result of results){
+		let isFile = extname(result) !== '';
+		let fullPath = resolve(current, result);
+		let display = relative(parse(root).dir, fullPath);
+
+		if (isFile){
+			content.files.push(display);
+			continue;
+		}
+
+		//Проверяем папку и всё вложенное в неё
+		content.folders.push(display);		
+		let innerContent = await readFolder(fullPath, root);
+		content.files = content.files.concat(innerContent.files);
+		content.folders = content.folders.concat(innerContent.folders);
+	}
+
+	return content;
+}
+
+
+getContent(fullPath);
